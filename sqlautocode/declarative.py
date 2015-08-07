4f29fdc3ec7e4856c70c5f14c7a4c0b493f5a687
Greg Meece <glmeece@gmail.com>
2015-08-06 23:30:21 -0500

    Update declarative.py
    Changed lines 14-17.
    
    From:
        from sqlalchemy.ext.declarative import _deferred_relationship
    except ImportError:
        #SA 0.5 support
        from sqlalchemy.ext.declarative import _deferred_relation as _deferred_relationship
    
    To:
        from sqlalchemy.ext.declarative import _deferred_relation as _deferred_relationship
    except ImportError:
        #SA 0.8 support
        from sqlalchemy.ext.declarative.clsregistry import _deferred_relationship
    
    Credit: http://www.childsplay.mobi/blog/?p=233

--- sqlautocode/declarative.py
+++ sqlautocode/declarative.py
@@ -11,10 +11,10 @@ from sqlalchemy import exc, and_
 from sqlalchemy import MetaData, ForeignKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 try:
-    from sqlalchemy.ext.declarative import _deferred_relationship
-except ImportError:
-    #SA 0.5 support
     from sqlalchemy.ext.declarative import _deferred_relation as _deferred_relationship
+except ImportError:
+    #SA 0.8 support
+    from sqlalchemy.ext.declarative.clsregistry import _deferred_relationship
     
 from sqlalchemy.orm import relation, backref, class_mapper, Mapper
 
